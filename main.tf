4005c66d3820c394e861ceadd4291023cbdc249f
Luis Quiñones <lpaandres2020@gmail.com>
2024-07-07 21:45:52 -0500

    feat: I'm in my way to hell

--- main.tf
+++ main.tf
@@ -15,31 +15,159 @@ module "luis_quinones_me_acm" {
 
 resource "aws_s3_object" "root" {
   bucket       = module.personal_s3.id
-  key          = "index.txt"
+  key          = "public/cdn/index.txt"
   content_type = "text/plain"
   source       = "assets/index.txt"
   etag         = filemd5("assets/index.txt")
 }
 
-resource "aws_cloudfront_origin_access_identity" "cdn" {
-  comment = "[tf] Origin access identity for luisnquin's CDN (S3)"
+module "wishlist" {
+  source = "terraform-aws-modules/cloudfront/aws"
+
+  aliases = ["wishlist.luisquinones.me"]
+
+  comment                       = "[tf] CloudFront distribution to put the wishlist :OOO"
+  enabled                       = true
+  is_ipv6_enabled               = true
+  price_class                   = "PriceClass_100"
+  retain_on_delete              = false
+  wait_for_deployment           = false
+  create_origin_access_identity = true
+  origin_access_identities = {
+    personal_bucket = "Origin access identity for my wishlist"
+  }
+
+  origin = {
+    personal_bucket = {
+      domain_name = module.personal_s3.bucket_domain_name
+      s3_origin_config = {
+        origin_access_identity = "personal_bucket"
+      }
+    }
+  }
+
+  geo_restriction = {
+    restriction_type = "blacklist"
+    locations        = ["RU", "CN", "IN", "SA", "FI"] # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+  }
+
+  default_cache_behavior = {
+    target_origin_id       = "personal_bucket"
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
+  }
+
+  ordered_cache_behavior = [
+    {
+      path_pattern           = "/public/wishlist/*"
+      target_origin_id       = "personal_bucket"
+      viewer_protocol_policy = "redirect-to-https"
+
+      allowed_methods = ["GET", "HEAD", "OPTIONS"]
+      cached_methods  = ["GET", "HEAD"]
+      compress        = true
+      query_string    = false
+    }
+  ]
+
+  custom_error_response = {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/"
+    error_caching_min_ttl = 10
+  }
+
+  viewer_certificate = {
+    acm_certificate_arn = module.luis_quinones_me_acm.acm_arn
+    ssl_support_method  = "sni-only"
+  }
 }
 
-data "aws_iam_policy_document" "cdn" {
+
+module "cdn" {
+  source = "terraform-aws-modules/cloudfront/aws"
+
+  aliases = ["cdnx.luisquinones.me"]
+
+  comment                       = "[tf] CloudFront distribution that acts as a personal CDN"
+  enabled                       = true
+  is_ipv6_enabled               = true
+  price_class                   = "PriceClass_100"
+  retain_on_delete              = false
+  wait_for_deployment           = false
+  create_origin_access_identity = true
+  origin_access_identities = {
+    personal_bucket = "Origin access identity for ${module.personal_s3.id}"
+  }
+
+  origin = {
+    personal_bucket = {
+      domain_name = module.personal_s3.bucket_domain_name
+      s3_origin_config = {
+        origin_access_identity = "personal_bucket"
+      }
+    }
+  }
+
+  geo_restriction = {
+    restriction_type = "blacklist"
+    locations        = ["RU", "CN", "IN", "SA", "FI"] # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+  }
+
+  default_cache_behavior = {
+    target_origin_id       = "personal_bucket"
+    viewer_protocol_policy = "allow-all"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+    query_string    = true
+  }
+
+  ordered_cache_behavior = [
+    {
+      path_pattern           = "/public/cdn/*"
+      target_origin_id       = "personal_bucket"
+      viewer_protocol_policy = "redirect-to-https"
+
+      allowed_methods = ["GET", "HEAD", "OPTIONS"]
+      cached_methods  = ["GET", "HEAD"]
+      compress        = true
+      query_string    = false
+    }
+  ]
+
+  custom_error_response = {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/"
+    error_caching_min_ttl = 10
+  }
+
+  viewer_certificate = {
+    acm_certificate_arn = module.luis_quinones_me_acm.acm_arn
+    ssl_support_method  = "sni-only"
+  }
+}
+
+data "aws_iam_policy_document" "public" {
   statement {
-    sid    = "AllowCloudFrontDistribution"
     effect = "Allow"
 
     principals {
       type = "AWS"
-      identifiers = [
-        aws_cloudfront_origin_access_identity.cdn.iam_arn
-      ]
+      identifiers = concat(
+        module.cdn.cloudfront_origin_access_identity_iam_arns,
+        module.wishlist.cloudfront_origin_access_identity_iam_arns
+      )
     }
 
     resources = [
-      "${module.personal_s3.arn}/resume.pdf",
-      "${module.personal_s3.arn}/index.txt"
+      "${module.personal_s3.arn}/public/cdn/*"
     ]
 
     actions = [
@@ -50,77 +178,7 @@ data "aws_iam_policy_document" "cdn" {
 
 resource "aws_s3_bucket_policy" "cdn" {
   bucket = module.personal_s3.id
-  policy = data.aws_iam_policy_document.cdn.json
-}
-
-resource "aws_cloudfront_distribution" "cdn" {
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "[tf] CloudFront distribution that acts as a personal CDN"
-  default_root_object = aws_s3_object.root.key
-
-  aliases = ["cdn.luisquinones.me"]
-
-  origin {
-    origin_id   = module.personal_s3.bucket_domain_name
-    domain_name = module.personal_s3.bucket_domain_name
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.cdn.cloudfront_access_identity_path
-    }
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = module.personal_s3.bucket_domain_name
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  custom_error_response {
-    error_code            = 403
-    response_code         = 200
-    response_page_path    = "/"
-    error_caching_min_ttl = 10
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "blacklist"
-      locations        = ["RU", "CN", "IN", "SA", "FI"] # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn            = module.luis_quinones_me_acm.acm_arn
-    cloudfront_default_certificate = false
-    ssl_support_method             = "sni-only"
-    minimum_protocol_version       = "TLSv1.2_2021"
-  }
-
-  wait_for_deployment = true
-
-  tags = {
-    ManagedWith = "terraform"
-    Goal        = "cdn"
-  }
-
-  depends_on = [
-    aws_cloudfront_origin_access_identity.cdn
-  ]
+  policy = data.aws_iam_policy_document.public.json
 }
 
 
@@ -130,12 +188,12 @@ data "cloudflare_zone" "luisquinones_me" {
 
 resource "cloudflare_record" "cdn" {
   type    = "CNAME"
-  name    = "cdn"
-  value   = aws_cloudfront_distribution.cdn.domain_name
+  name    = "cdnx"
+  value   = module.cdn.cloudfront_distribution_domain_name
   proxied = false
   zone_id = data.cloudflare_zone.luisquinones_me.zone_id
   depends_on = [
-    aws_cloudfront_distribution.cdn
+    module.cdn
   ]
   comment = "[tf] CNAME record for cdn"
 }
